eda5ece045f0c03a05cef53e4463b5f286a24f3b
romardoss <romassuper@gmail.com>
2022-10-28 12:09:13 +0300

    Linked List created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+        SinglyList<Integer> test = new SinglyList<>();
+        test.add(34);
+        for (int i = 0; i < 10; i++) {
+            test.add( (int) (Math.random() * 100) );
+        }
+        test.print();
     }
 }
---
+++ src/SinglyList.java
@@ -0,0 +1,42 @@
+public class SinglyList<T> {
+    private Node first;
+    private Node last;
+
+    public SinglyList(){
+        new Node(null, null);
+    }
+
+    private class Node{
+        T val;
+        Node next;
+
+        public Node(T obj, Node prev){
+            if(prev != null){
+                prev.next = this;
+                this.val = obj;
+            }
+            else{
+                this.val = obj;
+                first = this;
+                last = this;
+            }
+        }
+    }
+
+    public Node add(T obj){
+        last = new Node(obj, last);
+        if(first.val == null){
+            first = last;
+        }
+        return last;
+    }
+
+    public void print(){
+        Node current = first;
+        while (current != last){
+            System.out.printf(current.val.toString() + " -> ");
+            current = current.next;
+        }
+        System.out.printf(last.val.toString());
+    }
+}
